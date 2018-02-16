a69c8a7b2ce3d73781ec49c5868273b444d559e9
Lamartio <lamarti.danny@gmail.com>
2018-02-16 11:00:33 +0100

    Fixes naming in interceptor

--- src/main/kotlin/io/lamart/kosmos/CompositeInterceptor.kt
+++ src/main/kotlin/io/lamart/kosmos/CompositeInterceptor.kt
@@ -1,6 +1,6 @@
 package io.lamart.kosmos
 
-class CompositeInterceptor<T>(private var root: StoreSource<T>) : StoreSource<T> {
+class CompositeInterceptor<T>(private val root: StoreSource<T>) : StoreSource<T> {
 
     var router: (StoreSource<T>) -> StoreSource<T> = { it }
 
@@ -11,11 +11,11 @@ class CompositeInterceptor<T>(private var root: StoreSource<T>) : StoreSource<T>
     override fun invoke(action: Any) = router(root).invoke(action)
 
     fun add(router: (StoreSource<T>) -> StoreSource<T>): CompositeInterceptor<T> =
-            apply { this.router = combineRouters(this.router, router) }
+            apply { this.router = combineInterceptors(this.router, router) }
 
     companion object {
 
-        fun <T> combineRouters(
+        fun <T> combineInterceptors(
                 previous: (StoreSource<T>) -> StoreSource<T>,
                 next: (StoreSource<T>) -> StoreSource<T>
         ): (StoreSource<T>) -> StoreSource<T> = { it.let(previous).let(next) }
