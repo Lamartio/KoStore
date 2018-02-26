80326d69e9c1b37b10409ff2e4b46a128c16f61c
Lamartio <lamarti.danny@gmail.com>
2018-02-26 10:05:56 +0100

    Puts version back in their string

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,6 @@ import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.api.internal.file.pattern.PatternMatcherFactory.compile
 import org.gradle.script.lang.kotlin.*
 
-private val kotlinVersion = "1.2.21"
-
 plugins {
     `java-library`
 }
@@ -15,11 +13,11 @@ version = "0.9.0"
 repositories.jcenter()
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.2.21")
     testImplementation("junit:junit:4.12")
 }
 
 buildscript {
     repositories.mavenCentral()
-    dependencies.classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    dependencies.classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.2.21")
 }
