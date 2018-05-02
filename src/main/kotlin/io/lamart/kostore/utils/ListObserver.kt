f68bd4c6a2f6da43b66193c870aed3c2f0cbc43d
Lamartio <lamarti.danny@gmail.com>
2018-05-02 09:23:13 +0200

    WIP: Refactoring views

--- src/main/kotlin/io/lamart/kostore/utils/ListObserver.kt
+++ src/main/kotlin/io/lamart/kostore/utils/ListObserver.kt
@@ -2,7 +2,7 @@ package io.lamart.kostore.utils
 
 import io.lamart.kostore.Observer
 
-open class ListObserver<T>(list: MutableList<Observer<T>> = ArrayList()) : MutableList<Observer<T>> by list, Observer<T> {
+open class ListObserver<T>(list: MutableList<Observer<T>> = mutableListOf()) : MutableList<Observer<T>> by list, Observer<T> {
 
     override fun invoke(state: T) = forEach { it(state) }
 
