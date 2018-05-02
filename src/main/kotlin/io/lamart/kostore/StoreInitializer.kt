ffc799ec6705c64cefc5782a1039a55a427e7ffb
Lamartio <lamarti.danny@gmail.com>
2018-05-02 17:51:26 +0200

    Does some refactoring and extract the History Mock

--- src/main/kotlin/io/lamart/kostore/StoreInitializer.kt
+++ src/main/kotlin/io/lamart/kostore/StoreInitializer.kt
@@ -12,14 +12,14 @@ interface StoreInitializer<T> {
 fun <T, R> StoreInitializer<T>.compose(get: (T) -> R, reduce: T.(R) -> T, init: StoreInitializer<R>.() -> Unit) =
         compose(get, reduce).run(init)
 
-fun <T, R> StoreInitializer<T>.compose(map: (T) -> R, reduce: T.(R) -> T): StoreInitializer<R> = let { context ->
+fun <T, R> StoreInitializer<T>.compose(map: (T) -> R, reduce: T.(R) -> T): StoreInitializer<R> = let { initializer ->
     object : StoreInitializer<R> {
 
         override fun addReducer(reducer: Reducer<R>) =
-                reducer.compose(map, reduce).let(context::addReducer)
+                reducer.compose(map, reduce).let(initializer::addReducer)
 
         override fun addMiddleware(middleware: Middleware<R>) =
-                middleware.compose(map).let(context::addMiddleware)
+                middleware.compose(map).let(initializer::addMiddleware)
 
     }
 }
