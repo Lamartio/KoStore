dffd8a5777b0097db1c0b0b0b2eebeaf566dc481
Lamartio <lamarti.danny@gmail.com>
2018-06-08 15:42:53 +0200

    adds Optional composition

--- src/main/kotlin/io/lamart/kostore/initializers/compose.kt
+++ src/main/kotlin/io/lamart/kostore/initializers/compose.kt
@@ -43,3 +43,59 @@ inline fun <T, R, reified A : Any> compose(
                     }
 
         }
+
+
+/*
+    The optional family
+ */
+
+
+inline fun <T, R> OptionalInitializer<T>.composeOptional(
+        crossinline map: (T) -> R,
+        crossinline reduce: T.(R) -> T,
+        block: OptionalInitializer<R>.() -> Unit
+) = composeOptional(this, { filter(it) }, { filter(it) }, map, reduce).toOptionalInitializer().run(block)
+
+inline fun <T, R> OptionalInitializer<T>.composeOptional(
+        crossinline map: (T) -> R,
+        crossinline reduce: T.(R) -> T
+): OptionalInitializer<R> = composeOptional(this, { filter(it) }, { filter(it) }, map, reduce).toOptionalInitializer()
+
+inline fun <T, R, reified A : Any> FilteredOptionalInitializer<T, A>.composeFilteredOptional(
+        crossinline map: (T) -> R,
+        crossinline reduce: T.(R) -> T,
+        block: FilteredOptionalInitializer<R, A>.() -> Unit
+) = composeOptional(this, { filter(it) }, { filter(it) }, map, reduce).run(block)
+
+inline fun <T, R, reified A : Any> FilteredOptionalInitializer<T, A>.composeFilteredOptional(
+        crossinline map: (T) -> R,
+        crossinline reduce: T.(R) -> T
+): FilteredOptionalInitializer<R, A> = composeOptional(this, { filter(it) }, { filter(it) }, map, reduce)
+
+inline fun <T, R, reified A : Any> composeOptional(
+        initializer: FilteredOptionalInitializer<T, A>,
+        crossinline transformMiddleware: (FilteredMiddleware<R?, A>) -> FilteredMiddleware<R?, A>,
+        crossinline transformReducer: (FilteredReducer<R, A>) -> FilteredReducer<R, A>,
+        crossinline map: (T) -> R,
+        crossinline reduce: T.(R) -> T
+): FilteredOptionalInitializer<R, A> =
+        object : FilteredOptionalInitializer<R, A> {
+
+            override fun addMiddleware(middleware: FilteredMiddleware<R?, A>) =
+                    initializer.addMiddleware { getState, dispatch, action, next ->
+                        transformMiddleware(middleware).invoke(
+                                { getState()?.let(map) },
+                                dispatch,
+                                action,
+                                next
+                        )
+                    }
+
+            override fun addReducer(reducer: FilteredReducer<R, A>) =
+                    initializer.addReducer { state, action ->
+                        map(state)
+                                .let { transformReducer(reducer).invoke(it, action) }
+                                .let { state.reduce(it) }
+                    }
+
+        }
