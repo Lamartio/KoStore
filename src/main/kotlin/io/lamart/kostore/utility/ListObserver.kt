bf8b7bebbaab97716ce63253ea0e620aaf984d51
Lamartio <lamarti.danny@gmail.com>
2018-07-27 14:17:34 +0200

    copies the list to prevent concurrent modification

--- src/main/kotlin/io/lamart/kostore/utility/ListObserver.kt
+++ src/main/kotlin/io/lamart/kostore/utility/ListObserver.kt
@@ -4,6 +4,6 @@ import io.lamart.kostore.Observer
 
 open class ListObserver<T>(list: MutableList<Observer<T>> = mutableListOf()) : MutableList<Observer<T>> by list, Observer<T> {
 
-    override fun invoke(state: T) = forEach { observer -> observer(state) }
+    override fun invoke(state: T) = toList().forEach { observer -> observer(state) }
 
 }
